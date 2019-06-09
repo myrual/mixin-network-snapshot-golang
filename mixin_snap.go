f368446971d03795192fe9b2980d76634618c30c
lilin <lilin@myrual.me>
2019-06-09 23:34:32 +0800

    add const asset id

--- mixin_snap.go
+++ mixin_snap.go
@@ -149,6 +149,24 @@ func create_task(asset_id string, start_time2 time.Time, end_time2 time.Time, c 
 	}
 }
 
+const (
+	BTC_ASSET_ID  = "c6d0c728-2624-429b-8e0d-d9d19b6592fa"
+	EOS_ASSET_ID  = "6cfe566e-4aad-470b-8c9a-2fd35b49c68d"
+	USDT_ASSET_ID = "815b0b1a-2764-3736-8faa-42d694fa620a"
+	ETC_ASSET_ID  = "2204c1ee-0ea2-4add-bb9a-b3719cfff93a"
+	XRP_ASSET_ID  = "23dfb5a5-5d7b-48b6-905f-3970e3176e27"
+	XEM_ASSET_ID  = "27921032-f73e-434e-955f-43d55672ee31"
+	ETH_ASSET_ID  = "43d61dcd-e413-450d-80b8-101d5e903357"
+	DASH_ASSET_ID = "6472e7e3-75fd-48b6-b1dc-28d294ee1476"
+	DOGE_ASSET_ID = "6770a1e5-6086-44d5-b60f-545f9d9e8ffd"
+	LTC_ASSET_ID  = "76c802a2-7c88-447f-a93e-c29c9e5dd9c8"
+	SIA_ASSET_ID  = "990c4c29-57e9-48f6-9819-7d986ea44985"
+	ZEN_ASSET_ID  = "a2c5d22b-62a2-4c13-b3f0-013290dbac60"
+	ZEC_ASSET_ID  = "c996abc9-d94e-4494-b1cf-2a3fd3ac5714"
+	BCH_ASSET_ID  = "fd11b6e3-0b87-41f1-a41f-f0e9b49e5bf0"
+	XIN_ASSET_ID  = "c94ac88f-4671-3976-b60a-09064f1811e8"
+)
+
 func main() {
 	var start_time2 = time.Date(2018, 8, 11, 0, 0, 0, 0, time.UTC)
 	var end_time2 = time.Date(2018, 8, 12, 0, 0, 0, 0, time.UTC)
@@ -164,7 +182,7 @@ func main() {
 		private_key: private_key,
 	}
 
-	create_task("43d61dcd-e413-450d-80b8-101d5e903357", start_time2, end_time2, task_chan, 480)
+	create_task(BTC_ASSET_ID, start_time2, end_time2, task_chan, 480)
 	total_task := len(task_chan)
 	log.Println("go with ", total_task, " tasks")
 	total_snaps := 0
